15360676d65d7a0ae0ddc213a809a9c5b4e08be6
DanDiaconescu <andreimatei143@gmail.com>
2023-02-16 23:33:49 +0200

    Adaugat competitia si un schelet de day one
    Adaugat competitie speedrun cu api wrapper selfmade
    
    are numai functia de register creata momentan, trebuie adaugata si restul de leaderboards n shi
    
    Day one e restras din uz si va fi refacut

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import discord
 import bot_clan_members
 import clan_invite_embed
 import sys
+from os import environ
+from day_one import bot_register_dayone
+from api import register_concurs
 
 
 class UtilsBot(commands.Bot):
@@ -24,7 +27,7 @@ class UtilsBot(commands.Bot):
 
         # cmd_channel = await bot.fetch_channel(797387549089333268)
         # await cmd_channel.send(content='Bot online — Kind reminder sa restartezi embedul cu link-uri')
-        
+
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
@@ -40,6 +43,12 @@ class UtilsBot(commands.Bot):
 
 bot = UtilsBot()
 command_tree = bot.tree  # discord.app_commands.CommandTree(bot)
+
+'''
+pentru event
+'''
+event_processing = False
+event_last_user = None
 
 
 @command_tree.command(name='curatenie_generala', description='Admin-only',
@@ -87,6 +96,48 @@ async def privat_2(interaction: discord.Interaction):
         await interaction.response.send_message(content='Teapa', ephemeral=True)
 
 
+# @command_tree.command(name='dayone_register', description='Inscrie echipa pentru Day One — Lightfall',
+#                       guild=discord.Object(id=710809754057834496))
+# async def privat_3(interaction:discord.Interaction, membru_2: discord.User, membru_3: discord.User,
+#                    membru_4: discord.User, membru_5: discord.User, membru_6: discord.User):
+#     print(f'{"—"*10} \nGenerare embed register day one team')
+#     cmd_channel = await bot.fetch_channel(797387549089333268)
+#
+#     author = interaction.user
+#     args = [author, membru_2, membru_3, membru_4, membru_5, membru_6]
+#     # await interaction.response.defer()
+#     await interaction.response.send_message(content='Asteapta, te rog.', ephemeral=True)
+#     await bot_register_dayone.init(cmd_channel, args)
+
+
+@command_tree.command(name='event_register', description='Inscrie pentru competitia de speedrun — Lightfall',
+                      guild=discord.Object(id=710809754057834496))
+async def privat_4(interaction:discord.Interaction, link: str):
+    print(f'{"—"*10} Inscriere noua pentru competitie {interaction.user}')
+    global event_processing, event_last_user
+
+    author = interaction.user
+
+    if event_processing and event_last_user != author.id:
+        await interaction.response.send_message(content="Momentan comanda este folosita de alt utilizator, te rog sa revi in cateva momente.",ephemeral=True)
+        return
+    event_processing = True
+    event_last_user = author.id
+
+    author_name = author.nick
+    if not author_name:
+        author_name = author.name
+
+    cmd_channel = await bot.fetch_channel(797387549089333268)
+
+    # await interaction.response.defer()
+    # await interaction.response.send_message(content='Asteapta, te rog.', ephemeral=True)
+    try:
+        await register_concurs.init(interaction, author_name, link, cmd_channel)
+    finally:
+        event_processing = False
+
+
 @bot.event
 async def on_member_join(member):
     print(f"{'—'*5} Generare mesaj membru nou - {member.name} {'—'*5}")
@@ -96,7 +147,7 @@ Te rog să mergi pe <#938290015195238400> și să urmezi pașii de acolo.
 Dacă dai join pe unul dintre clanuri, te rog să dai tag responsabililor de clan pe <#938294344853647431>. 
 Registerul cu Warmind (Charlemange) este obligatoriu in cadrul comunitatii noastre.
 Dacă întâmpini probleme, te rog să contactezi un administrator in thread-ul de mai jos. '''
-    new_message = await welcome_channel.send(content=welcome_txt.format(member.mention).replace('\n', ''))
+    # new_message = await welcome_channel.send(content=welcome_txt.format(member.mention).replace('\n', ''))
 
     # server = await bot.fetch_guild(710809754057834496)
     # gatekeep = server.get_role(729027061322350762)
@@ -104,8 +155,8 @@ Dacă întâmpini probleme, te rog să contactezi un administrator in thread-ul 
     # admin = server.get_role(710818161867620412)
 
     admin_list = ['<@&729027061322350762>', '<@&790256564110884864>', '<@&710818161867620412>']
-    new_thread = await new_message.create_thread(name=f'Support Thread - {member.name}')
-    await new_thread.send(content=f'Dacă întâmpini probleme, te rog să ne lași un mesaj aici și te vom asista în cel mai scurt timp posibil. {member.mention} {" ".join(admin_list)}')
+    # new_thread = await new_message.create_thread(name=f'Support Thread - {member.name}')
+    # await new_thread.send(content=f'Dacă întâmpini probleme, te rog să ne lași un mesaj aici și te vom asista în cel mai scurt timp posibil. {member.mention} {" ".join(admin_list)}')
 
 
 @tasks.loop(minutes=60)
@@ -141,4 +192,7 @@ async def do_refresh_bot():
 # TOKEN = str(environ.get('TOKEN'))  # sus la run dropdown file -> edit config -> enviroment variables -> TOKEN
 if len(sys.argv) > 1:
     TOKEN = sys.argv[1]
+    bot.run(TOKEN)
+else:
+    TOKEN = str(environ.get('TOKEN_TEST'))
     bot.run(TOKEN)
